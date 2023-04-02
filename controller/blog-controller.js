c748707d8df82e83ba905f1c0e48b5a90797c0c3
AbhijeetVashishtha <abhijeetvashishth13@gmail.com>
2023-04-02 14:13:55 +0530

    Functionality for Posting Blog

--- controller/blog-controller.js
+++ controller/blog-controller.js
@@ -14,3 +14,21 @@ exports.getAllBlogs = async(req,res) => {
         return res.status(500).json({message: "Something went wrong"});
     }
 }
+
+exports.postBlog = async (req,res) => {
+    try{
+        const {title, description, image, user} = req.body;
+        const blog = new Blog({
+            title,
+            description,
+            image,
+            user
+        });
+        await blog.save();
+        return res.status(200).json({blog}); 
+    }
+    catch(err) {
+        console.log(err);
+        return res.status(500).json({message: "Something went Wrong"});
+    }
+}
--- route/blog-routes.js
+++ route/blog-routes.js
@@ -6,4 +6,6 @@ const blogController = require('../controller/blog-controller');
 
 router.get("/", blogController.getAllBlogs);
 
+router.post("/addBlog", blogController.postBlog);
+
 module.exports = router;
